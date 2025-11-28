8e02c72644ead553c5115b55b1935adb9f4a9ce3
savitriianantapur-dot <savitriianantapur@gmail.com>
2025-11-28 12:32:58 +0530

    Update ApiGatewayServiceApplication.java

--- ApiGateway-Service/src/main/java/com/api/gateway/ApiGatewayServiceApplication.java
+++ ApiGateway-Service/src/main/java/com/api/gateway/ApiGatewayServiceApplication.java
@@ -11,6 +11,7 @@ public class ApiGatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayServiceApplication.class, args);
 		int i=10;
+		String s="Savitri";
 	}
 
 }
